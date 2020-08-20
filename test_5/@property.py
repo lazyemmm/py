c9e995eacfdbeda4b6e5ac40ae7c55720789dd6e
wuqibao-at-567022188985 <fightboy_wu@163.com>
2020-08-20 18:40:20 +0800

    test_5

---
+++ test_5/@property.py
@@ -0,0 +1,37 @@
+"""
+Python内置的@property装饰器就是负责把一个方法变成属性调用的
+把一个getter方法变成属性，只需要加上@property就可以了
+此时，@property本身又创建了另一个装饰器@score.setter，负责把一个setter方法变成属性赋值，于是，我们就拥有一个可控的属性操作
+注意到这个神奇的@property，我们在对实例属性操作的时候，就知道该属性很可能不是直接暴露的，而是通过getter和setter方法来实现的
+"""
+class Screen(object):
+    pass
+    @property
+    def width(self):
+        return self._witdh
+
+    @width.setter
+    def width(self, width):
+        self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        self._height = height
+
+    @property
+    def resolution(self):
+        return 786432
+
+
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
